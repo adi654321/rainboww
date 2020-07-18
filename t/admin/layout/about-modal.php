6ea680bd25ace1054dc7aa14de8dc7012c0f0726
adi654321 <68476546+adi654321@users.noreply.github.com>
2020-07-18 23:19:14 +0600

    Add files via upload

---
+++ t/admin/layout/about-modal.php
@@ -0,0 +1,9 @@
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>About Rainbow warrior</h4>
+     
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>
